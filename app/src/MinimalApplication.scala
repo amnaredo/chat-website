e1634e65ba1437adbc7bb5219346a3e7bd85c569
Alejandro <amnaredo@gmail.com>
2021-07-27 18:29:17 +0200

    dynamic page rendering based on messages data, form handling

--- app/src/MinimalApplication.scala
+++ app/src/MinimalApplication.scala
@@ -1,7 +1,7 @@
 package app
 import scalatags.Text.all._
 object MinimalApplication extends cask.MainRoutes{
-
+  var messages = Vector(("alice", "Hello World!"), ("bob", "I am cow, hear me moo"))
   val bootstrap = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.css"
 
   @cask.get("/")
@@ -12,21 +12,22 @@ object MinimalApplication extends cask.MainRoutes{
         div(cls := "container")(
           h1("Scala Chat!"),
           div(
-            p(b("alice"), " ", "Hello World!"),
-            p(b("bob"), " ", "I am cow, hear me moo")
+            for ((name, msg) <- messages) yield p(b(name), " ", msg)
           ),
-          div(
-            input(`type` := "text", placeholder := "User name"),
-            input(`type` := "text", placerolder := "Write a message!") 
+          form(action := "/", method := "post")(
+            input(`type` := "text", name := "name", placeholder := "User name"),
+            input(`type` := "text", name := "msg", placerolder := "Write a message!"),
+            input(`tyoe` := "submit") 
           )
         )
       )
     )
   )
 
-  @cask.post("/do-thing")
-  def doThing(request: cask.Request) = {
-    request.text().reverse
+  @cask.postForm("/")
+  def postChatMsg(name: String, msg: String) = {
+    messages = messages :+ (name -> msg)
+    hello()
   }
 
   initialize()
