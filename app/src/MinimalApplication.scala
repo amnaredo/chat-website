1d14cec8193ec24c43537f6d958b1770a110431b
Alejandro <amnaredo@gmail.com>
2021-07-27 18:35:09 +0200

    added simple form validation

--- app/src/MinimalApplication.scala
+++ app/src/MinimalApplication.scala
@@ -5,14 +5,15 @@ object MinimalApplication extends cask.MainRoutes{
   val bootstrap = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.css"
 
   @cask.get("/")
-  def hello() = doctype("html")(
+  def hello(errorOpt: Option[String] = None) = doctype("html")(
     html(
       head(link(rel := "stylesheet", href := bootstrap)),
       body(
         div(cls := "container")(
           h1("Scala Chat!"),
           div(
-            for ((name, msg) <- messages) yield p(b(name), " ", msg)
+            for ((name, msg) <- messages) yield p(b(name), " ", msg),
+            for (error <- errorOpt) yield i(color.red)(error)
           ),
           form(action := "/", method := "post")(
             input(`type` := "text", name := "name", placeholder := "User name"),
@@ -26,8 +27,13 @@ object MinimalApplication extends cask.MainRoutes{
 
   @cask.postForm("/")
   def postChatMsg(name: String, msg: String) = {
-    messages = messages :+ (name -> msg)
-    hello()
+    // if (name != "" && msg != "") messages = messages :+ (name -> msg)
+    if (name == "") hello(Some("Name cannot be empty"))
+    else if (msg == "") hello(Some("Message cannot be empty"))
+    else {
+      messages = messages :+ (name -> msg)
+      hello()
+    }
   }
 
   initialize()
