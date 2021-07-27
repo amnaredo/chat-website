d814eafdbb5dd6e7c196e27789320a267ac5afff
Alejandro <amnaredo@gmail.com>
2021-07-27 13:17:04 +0200

    serving HTML

--- app/src/MinimalApplication.scala
+++ app/src/MinimalApplication.scala
@@ -1,9 +1,21 @@
 package app
+import scalatags.Text.all._
 object MinimalApplication extends cask.MainRoutes{
+
+  val bootstrap = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.css"
+
   @cask.get("/")
-  def hello() = {
-    "Hello World, my friend!"
-  }
+  def hello() = doctype("html")(
+    html(
+      head(link(rel := "stylesheet", href := bootstrap)),
+      body(
+        div(cls := "container")(
+          h1("Hello!"),
+          p("World")
+        )
+      )
+    )
+  )
 
   @cask.post("/do-thing")
   def doThing(request: cask.Request) = {
--- build.sc
+++ build.sc
@@ -4,6 +4,7 @@ import mill._, scalalib._
 object app extends ScalaModule{
   def scalaVersion = "2.13.2"
   def ivyDeps = Agg(
+    ivy"com.lihaoyi::scalatags:0.9.1",
     ivy"com.lihaoyi::cask:0.7.4",
   )
   object test extends Tests{
